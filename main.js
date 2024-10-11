ad9b227aa3e729c0874f9bd08948e274034724ae
vaibhav9852 <vaibhav.s@webkorps.com>
2024-10-12 02:17:17 +0530

    remove setTimeout

--- main.js
+++ main.js
@@ -4,24 +4,71 @@ async function fetchPhotos(){
 
         let responce = await fetch(`https://fakestoreapi.com/products`)
         let data = await responce.json()
-        console.log('data',data)
-        const card = document.querySelector('.card')
-        data.map((item) =>{
-             const img = document.createElement('img')
-             img.src = `${item.image}`
-             img.alt = `image`
-             card.appendChild(img)
-            const p = document.createElement('p')
-            p.innerText = `${item.category}` 
-            card.appendChild(p)
+        const cards = document.querySelector('.cards')
+        
+        data.map((item,ind) =>{
+            const div = document.createElement('div')
+            div.setAttribute('class','card')
+            const img = document.createElement('img')
+            img.src = `${item.image}`
+            img.alt = `image`
+            div.appendChild(img)
+            const h2 = document.createElement('h2')
+            h2.innerText = `${item.category}` 
+            div.appendChild(h2)
             const btn = document.createElement('button')
             btn.innerText = 'Learn more  ->'
-            card.appendChild(btn)
+            btn.setAttribute('class',`${ind}-${item.description?.slice(0,15)}`)
+            div.appendChild(btn)
+            cards.appendChild(div)
         })
 
-    }catch(err){
-        console.log('error',err)
+        // hanle model logic 
+
+        const modelOuter = document.querySelector('.model-outer')
+        const modelInner = document.querySelector('.model-inner')
+       function handleButtonClick(event) {
+          event.preventDefault();
+        const button = event.currentTarget;
+        
+        const card = button.closest('.card')
+  
+         const imgSrc = card.querySelector('img').src
+         const info = button.getAttribute('class').split('-')[1]
+         const name = card.querySelector('h2').textContent;
+   
+         // populate the model 
+         modelInner.innerHTML = `
+         <img src="${imgSrc.replace('400','400')}" alt="${name}"/>
+         <p>${name}</p>
+         `
+         modelOuter.classList.add('open')
+  
+         modelOuter.addEventListener('click',function(event){
+          const isOutside = !event.target.closest('.inner-model') // it will give null value every time
+          if(isOutside){
+              closeModel()
+          }
+         })  
+      }
+  
+      function closeModel(){
+          modelOuter.classList.remove('open');
+      }
+      
+      const cardButton =  document.querySelectorAll('.card button') // querySelectorAll('.card button')
+      cardButton.forEach((btn) =>{
+           btn.addEventListener('click',handleButtonClick)
+      })
+
+    }catch(error){
+        console.log('error while fetch data',error)   
     }
 }
 
-fetchPhotos() 
+fetchPhotos() ; 
+
+
+
+
+
